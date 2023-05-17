6c3273a1c26cd3557eff7630c4a2b0ee4821c18e
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-17 22:31:56 +0600

    project setup done

---
+++ src/routers/Routes.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      
+    },
+  ]);
+  
+
+export default router;

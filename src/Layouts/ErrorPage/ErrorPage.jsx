b8012c1be95a2b1fa7f4a94ad9beb3f48593c36a
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-18 02:27:56 +0600

    404 not found error page done

---
+++ src/Layouts/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ErrorPage = () => {
+    const navigate = useNavigate();
+    const handleBtnBackPage =()=>{
+        navigate(-1);
+    }
+    return (
+        <div>
+            <div className='mt-10 flex items-center justify-center'>
+            <h1 className=' text-4xl font-semibold'>404 Page Not Found</h1><br />
+            
+        </div>
+        <div className='flex items-center justify-center'>
+
+        <button className='px-3 py-2 text-white bg-slate-400 rounded-lg mt-10' onClick={handleBtnBackPage} >Go back</button>
+        </div>
+        </div>
+        
+    );
+};
+
+export default ErrorPage;
--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -4,11 +4,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layouts/Main/Main';
 import Login from '../Layouts/Login/Login';
 import Registration from '../Layouts/Registration/Registration';
+import ErrorPage from '../Layouts/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
     {
       path: "/",
       element:<Main></Main>,
+      errorElement:<ErrorPage></ErrorPage>,
       children:[
         {
             path:"login",

da683a3d1920133ac930409bf49700684326f42e
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-19 10:44:44 +0600

    banner section modified

--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -1,53 +1,47 @@
-
-import img1 from "../../assets/images/img2.jpg"
-import img2 from "../../assets/images/img1.jpg"
-import img3 from "../../assets/images/img3.webp"
-import img4 from "../../assets/images/img4.webp"
-import img5 from "../../assets/images/img5.jpeg"
-import Collection from "./Collection"
+import img1 from "../../assets/images/img2.jpg";
+import img2 from "../../assets/images/img1.jpg";
+import img3 from "../../assets/images/img3.webp";
+import img4 from "../../assets/images/img4.webp";
+import img5 from "../../assets/images/img5.jpeg";
+import Collection from "./Collection";
 const Home = () => {
   return (
-    <div>
-      <div className="carousel carousel-center  lg:h-[100vh] p-4 space-x-4 bg-neutral-300 rounded-box mx-auto my-10">
-        <div className="carousel-item">
-          <img
-            src={img1}
-            className="rounded-box w-[700px]  "
-          />
+    <div className="">
+      <div className=" lg:flex items-center justify-between gap-10 ">
+        <div className=" px-12 font-semibold text-left">
+          <h1 className=" text-7xl">
+            Collect Super rare <span className=" text-amber-500">digital</span>{" "}
+            Assets Today!
+          </h1>
+          <div className=" lg:flex items-center py-6 lg:gap-10">
+            <button className="bg-amber-500 hover:bg-blue-500 text-white font-semibold py-3 px-4 rounded-full">
+              Explore
+            </button>
+            <button className="bg-blue-500 hover:bg-amber-500 text-white font-semibold py-3 px-4 rounded-2xl">
+              Connect Wallet
+            </button>
+          </div>
         </div>
-        <div className="carousel-item ">
-          <img
-            src={img2}
-            className="rounded-box w-[700px]"
-          />
+        <div className="carousel carousel-center lg:w-[1000px]  lg:h-[70vh] p-4 space-x-4 bg-neutral-300 rounded-box  my-10">
+          <div className="carousel-item">
+            <img src={img1} className="rounded-box w-[400px]  " />
+          </div>
+          <div className="carousel-item ">
+            <img src={img2} className="rounded-box w-[400px]" />
+          </div>
+          <div className="carousel-item ">
+            <img src={img3} className="rounded-box w-[400px]" />
+          </div>
+          <div className="carousel-item ">
+            <img src={img2} className="rounded-box w-[400px]" />
+          </div>
+          <div className="carousel-item ">
+            <img src={img5} className="rounded-box w-[400px]" />
+          </div>
+          <div className="carousel-item ">
+            <img src={img4} className="rounded-box w-[400px]" />
+          </div>
         </div>
-        <div className="carousel-item ">
-          <img
-            src={img3}
-            className="rounded-box w-[700px]"
-          />
-        </div>
-        <div className="carousel-item ">
-          <img
-            src={img2}
-            className="rounded-box w-[700px]"
-          />
-        </div>
-        <div className="carousel-item ">
-          <img
-            src={img5}
-            className="rounded-box w-[700px]"
-          />
-        </div>
-        <div className="carousel-item ">
-          <img
-            src={img4}
-            className="rounded-box w-[700px]"
-          />
-        </div>
-        
-
-        
       </div>
 
       <Collection></Collection>
